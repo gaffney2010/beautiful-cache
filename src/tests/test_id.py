8b4d332450b04c150acb1fe92254a75c6d627c48
T.J. Gaffney <gaffney.tj@gmail.com>
2022-04-10 06:17:05 -0500

    Add test

--- src/tests/test_id.py
+++ src/tests/test_id.py
@@ -7,7 +7,34 @@ from shared_types import *
 
 class TestEndToEnd(unittest.TestCase):
     def test_happy_path(self):
-        pass
+        example_html = Html(
+            """
+        <html><body>
+        <x>
+            <y>First y</y>
+            <y>Second y <z>z</z></y>
+        </x>
+        </body</html>
+        """
+        )
+
+        engine = (
+            PolicyEngineGenerator().add_website(Url("test_url"), example_html).build()
+        )
+
+        soup = bc.BeautifulCache(Url("test_url"), Policy("test_policy"), engine=engine)
+        x = soup.find("x")
+        y1 = x.find_all("y")[0]
+        y2 = x.find_all("y")[1]
+        z = y2.find("z")
+
+        self.assertEqual(x.id(), "html:0/body:0/x:0")
+        self.assertEqual(y1.id(), "html:0/body:0/x:0/y:0")
+        self.assertEqual(y2.id(), "html:0/body:0/x:0/y:1")
+        self.assertEqual(z.id(), "html:0/body:0/x:0/y:1/z:0")
 
     def test_find_params(self):
+        pass
+
+    def test_cache(self):
         pass
