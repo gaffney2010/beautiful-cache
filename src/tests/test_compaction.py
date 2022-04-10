aa5a093c070d29643e99a027d7a3519e74be6c94
T.J. Gaffney <gaffney.tj@gmail.com>
2022-04-10 06:57:21 -0500

    Add unimplemented tests

--- src/tests/test_compaction.py
+++ src/tests/test_compaction.py
@@ -7,6 +7,42 @@ from shared_types import *
 
 class TestCompaction(unittest.TestCase):
 
-    # No compaction
-    def test_happy_path(self):
+    def test_all_happy_path(self):
+        # TODO: 
+        pass
+
+    def test_fat_happy_path(self):
+        # TODO: 
+        pass
+
+    def test_thin_happy_path(self):
+        # TODO: 
+        pass
+
+    def test_use_yaml(self):
+        # TODO: 
+        pass
+
+    def test_ts_ties(self):
+        # TODO: 
+        pass
+
+    def test_transaction(self):
+        # TODO: 
+        pass
+
+    def test_multiple_policies(self):
+        # TODO: 
+        pass
+
+    def test_erase_entire_old_before_any_new(self):
+        # TODO: 
+        pass
+
+    def test_after_materialize_compact_all(self):
+        # TODO: 
+        pass
+
+    def test_bump_only_materialized_file(self):
+        # TODO: 
         pass
