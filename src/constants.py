842bb53323278084ef04ab169db51435b8879e07
T.J. Gaffney <gaffney.tj@gmail.com>
2022-04-19 00:06:03 -0500

    Add constants

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+"""Some constants that will be shared potentially across files."""
+
+import os
+
+# If downloading, you need to set this directory to equal the directory that
+#  houses this file.
+from computer_constants import TOP_LEVEL_DIR
+
+LOGGING_DIR = os.path.join(TOP_LEVEL_DIR, "logging")
