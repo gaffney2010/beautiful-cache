19643cab20cb949a247289c5f45b61217da95d55
T.J. Gaffney <gaffney.tj@gmail.com>
2022-04-20 17:34:00 -0500

    Cache html

--- example_use_case/star_treks.py
+++ example_use_case/star_treks.py
@@ -13,8 +13,28 @@ from tabulate import tabulate
 import bc
 from compaction import compact
 from constants import *
-from policy_engine_class import ConcreteBcEngine
+from policy_engine_class import *
 from shared_types import *
+
+# Make a cached URL reader
+class CachedUrlReader(ConcreteUrlReader):
+    def _read(self, url: Url) -> Html:
+        key = str(url).replace("/", "") + ".data"
+        if os.path.exists(os.path.join("example_use_case/data/raw_html", key)):
+            with open(os.path.join("example_use_case/data/raw_html", key), "r") as f:
+                return Html(f.read())
+
+        with WebDriver() as driver:
+            page_text = self._read_url_to_string_helper(url, self.driver)
+
+        with open(os.path.join("example_use_case/data/raw_html", key), "w") as f:
+            f.write(page_text)
+
+        return Html(page_text)
+
+
+engine = ConcreteBcEngine
+engine.url_reader = CachedUrlReader()
 
 print("Hello")
 
@@ -44,7 +64,7 @@ tos_index = "https://trekfanfiction.net/category/the-original-series/#14"
 for _ in range(10):
     print(f"{_} @ {time.monotonic()}")
     # TODO: A signature that doesn't require Url-type
-    soup = bc.BeautifulCache(Url(tos_index), policy)
+    soup = bc.BeautifulCache(Url(tos_index), policy, engine=engine)
     links = list()
     for a in soup.find_all("a", {"rel": "bookmark"}):
         links.append(a.materialize()["href"])
@@ -64,12 +84,13 @@ def summary():
     df = pd.DataFrame(df_rows)
     print(tabulate(df))
 
+
 summary()
 
 for _ in range(2):
     for link in links:
         print(f"Downloading {link}...")
-        soup = bc.BeautifulCache(Url(link), policy)
+        soup = bc.BeautifulCache(Url(link), policy, engine=engine)
         for p in soup.find_all("p"):
             if str(p.tag).find("Kirk") != -1:
                 p.materialize()
@@ -77,7 +98,7 @@ for _ in range(2):
 summary()
 
 print("Compacting...")
-compact(policy, {"max_bytes": 2_500_000, "strategy": "thin"})
+compact(policy, {"max_bytes": 2_500_000, "strategy": "thin"}, engine=engine)
 
 summary()
 
